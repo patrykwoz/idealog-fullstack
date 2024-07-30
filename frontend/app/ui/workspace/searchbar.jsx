9fae63dedebb3dd0ebf4661c69d00ab59e54daf1
Patryk Wozniczka <patryk.wozniczka@gmail.com>
2024-07-30 15:44:35 -0400

    Build workspace

--- frontend/app/ui/workspace/searchbar.jsx
+++ frontend/app/ui/workspace/searchbar.jsx
@@ -1,18 +1,25 @@
 import {
-    PencilSquareIcon,
-    ArrowRightCircleIcon,
-    ArrowRightIcon,
+    MagnifyingGlassIcon,
     ArrowUpIcon,
 } from '@heroicons/react/24/outline';
+import styles from './searchbar.module.css';
 
 export default function SearchBar() {
     return (
         <>
 
-            <form action="" className="searchbarForm">
-                <input type="text" id='search' />
-                <button className="searchbarFormButton">
-                    <ArrowUpIcon className="searchbarFormIcon" />
+            <form action="" className={styles.searchbarForm}>
+                <label htmlFor='search' className={styles.searchbarFormLabel}>
+                    <MagnifyingGlassIcon className={styles.magnifyingGlassIcon} />
+
+                </label>
+                <input
+                    className={styles.searchbarFormInput}
+                    type="text"
+                    id='search'
+                    placeholder='Start typing to search your graph...' />
+                <button className={styles.searbarFormButton}>
+                    <ArrowUpIcon className={styles.searchbarButtonIcon} />
                 </button>
             </form>
 
